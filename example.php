4da6f7aea4045f085bc4f6ef890d8f6af93d98f2
sugata <sugatabhar@yahoo.com>
2018-03-29 14:33:50 +0530

    Added access control origin * and form post variable

--- example.php
+++ example.php
@@ -1,12 +1,15 @@
 <?php
+header('Access-Control-Allow-Origin: *'); 
 
 include __DIR__ . '/vendor/autoload.php';
 
-$cssContent = <<<EOF
+/*$cssContent = <<<EOF
 a[href^="javascript:"]:after {
     border: 0;
 }
-EOF;
+EOF;*/
+
+$cssContent = htmlentities($_POST['scss'], ENT_QUOTES, "UTF-8");
 
 $css2scssParser = new \Ortic\Css2Scss\Css2Scss($cssContent);
 echo $css2scssParser->getScss();
